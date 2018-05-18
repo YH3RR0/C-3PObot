93d4cabf8140a76b1f2b472cb6db4168c0808b09
Peter Cho <cho6087@ps598679.dreamhostps.com>
2018-05-18 13:55:25 -0700

    adding images

---
+++ js/figures.js
@@ -0,0 +1,6 @@
+document.write('
+    \<figure class="figure">\
+        \<img src="https://i.imgur.com/CQ20utR.png" alt="!emo vira">\
+        \<figcaption class="text-xs-center">!emo vira</figcaption>\
+    \</figure>\
+');
