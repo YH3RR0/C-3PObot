7c544a6b58dd46b12bf6a2fe47ee4a9e89a64c8d
Peter Cho <cho6087@ps598679.dreamhostps.com>
2018-05-18 14:01:26 -0700

    adding images

--- js/figures.js
+++ js/figures.js
@@ -1,6 +1,1 @@
-document.write('
-    <figure class="figure">
-        <img src="https://i.imgur.com/CQ20utR.png" alt="!emo vira">
-        <figcaption class="text-xs-center">!emo vira</figcaption>
-    </figure>
-');
+document.write('<figure class="figure"><img src="https://i.imgur.com/CQ20utR.png" alt="!emo vira"><figcaption class="text-xs-center">!emo vira</figcaption></figure>');
